455e97c8e66393dbf6ce11bd9c51287065faea7e
Reece Buyan <rbuyan@uci.edu>
2024-02-22 18:03:54 -0800

    Refunctioned it as an app navigator

--- front_end/App.js
+++ front_end/App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import AppNavigator from './components/AppNavigator.js';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi whats up!</Text>
-      <StatusBar style="auto" />
-    </View>
+    console.log("App Started"),
+    <AppNavigator />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
