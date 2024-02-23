9936394ca64b8bc0cf254474fa13b4e2bf4c667f
Reece Buyan <rbuyan@uci.edu>
2024-02-22 18:05:33 -0800

    Created a JS file that navigates through the different pages

---
+++ front_end/components/AppNavigator.js
@@ -0,0 +1,21 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import WelcomeScreen from './WelcomeScreen.js';
+import BasicInfoInput from './BasicInfoInput.js';
+import AdvancedInfoInput from './AdvancedInfoInput.js';
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="BasicInfoInput" component={BasicInfoInput} options={{ headerShown: false }} />
+        <Stack.Screen name="AdvancedInfoInput" component={AdvancedInfoInput} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
