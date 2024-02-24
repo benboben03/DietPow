e19001aed22f4e8dcdab2f92f6d5e7fe8198ee77
Reece Buyan <rbuyan@uci.edu>
2024-02-23 17:08:06 -0800

    Created functionality for selecting the user's age

--- front_end/components/pickers/AgePicker.js
+++ front_end/components/pickers/AgePicker.js
@@ -13,13 +13,35 @@ const OPTIONS = [1, 2, 3 ,4, 5, 6, 7, 8, 9, 10,
 const WIDTH = Dimensions.get('window').width;
 const HEIGHT = Dimensions.get('window').height;
 const AgePicker = (props) => {
+    
+    const onPressItem = (option) => {
+        props.changeModalVisibility(false);
+        props.setData(option);
+    }
+    
+    const option = OPTIONS.map((item, index) => {
+        return (
+            <TouchableOpacity
+                style={styles.option}
+                key={index}
+                onPress={() => onPressItem(option)}
+            >
+                <Text style={styles.text}>
+                    {item}
+                </Text>
+            </TouchableOpacity>
+        )
+    })
+
     return (
         <TouchableOpacity
             onPress={() => props.changeModalVisibility(false)}
             style={styles.container}
         >
             <View style={[styles.modal, {width: WIDTH - 20, height: HEIGHT/2}]}>
-
+                <ScrollView>
+                    {}
+                </ScrollView>
             </View>
         </TouchableOpacity>
     )
@@ -34,6 +56,14 @@ const styles = StyleSheet.create({
     modal: {
         backgroundColor: 'white',
         borderRadius: 10
+    },
+    option: {
+        alignItems: 'flex-start',
+    },
+    text: {
+        margin: 20, 
+        fontSize: 20,
+        fontWeight: 'bold'
     }
 })
 
