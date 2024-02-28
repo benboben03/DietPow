3377aa4615688bb28cc76853e91caff0c15ec272
Ryan Zhang <zyratlo@gmail.com>
2024-02-28 15:26:53 -0800

    reformatted files to have consistent indentation

--- front_end/App.js
+++ front_end/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import AppNavigator from './components/AppNavigator.js';
 
 export default function App() {
-  return (
-    console.log("App Started"),
-    <AppNavigator />
-  );
+    return (
+        console.log("App Started"),
+            <AppNavigator/>
+    );
 }
