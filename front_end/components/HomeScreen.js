be078824a3ea82f322c628acbf08ae23011d2c17
Ryan Zhang <zyratlo@gmail.com>
2024-03-01 11:03:42 -0800

    created skeleton framework

--- front_end/components/HomeScreen.js
+++ front_end/components/HomeScreen.js
@@ -1,11 +1,21 @@
-import {StyleSheet, Text, SafeAreaView} from 'react-native';
+import {StyleSheet, Text, SafeAreaView, View} from 'react-native';
 
 const HomeScreen = () => {
     console.log("HomeScreen rendered");
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text> HomeScreen TODO </Text>
+            <View style={styles.topSection}>
+                <Text style={styles.quoteHeaderText}>Today's Quote</Text>
+
+                {/*TODO pull quotes from database*/}
+                <Text style={styles.quoteText}>"Take care of your body, it's the only place you have to live in."{"\n"}</Text>
+
+                <Text style={styles.titleText}>Your Health Home</Text>
+            </View>
+            <View style={styles.bottomSection}>
+                <Text>HomeScreen TODO</Text>
+            </View>
         </SafeAreaView>
     );
 }
@@ -13,10 +23,35 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#11BCF5',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#11BCF5'
     },
+    topSection: {
+        backgroundColor: '#11BCF5',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        padding: 20
+    },
+    bottomSection: {
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    quoteHeaderText: {
+        fontSize: 30,
+        color: 'white',
+        fontWeight: 'bold',
+        textDecorationLine: 'underline'
+    },
+    quoteText: {
+        fontSize: 20,
+        color: 'white'
+    },
+    titleText: {
+        fontSize: 40,
+        color: 'white',
+        fontWeight: 'bold'
+    }
 })
 
 export default HomeScreen;
