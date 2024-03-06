606e21c7428bad852dad973bef05a74d9c4a3839
Ben Boben <bboben@uci.edu>
2024-03-05 21:45:23 -0800

    user database semi completed

---
+++ back_end/database/models.py
@@ -0,0 +1,70 @@
+from pydantic import BaseModel, EmailStr
+from typing import List, Dict
+from datetime import datetime, date
+from enum import Enum
+
+# Track User Info
+class User(BaseModel):
+    # email: EmailStr
+    email: str
+    name: str
+    age: int
+    weight: float
+    height: float
+    activity_level: str
+    goal: str
+
+class TrackedWeight(BaseModel):
+    t_date: date
+    current_weight: float
+
+class UserGoalTrack(BaseModel):
+    email: EmailStr
+    tracked_weights: List[TrackedWeight] = []
+
+
+# Track meals
+class MealTime(str, Enum):
+    BREAKFAST = 'Breakfast'
+    LUNCH = 'Lunch'
+    DINNER = 'Dinner'
+    SNACKS = 'Snacks'
+
+class EachFood(BaseModel):
+    meal_time: MealTime
+    description: str
+    calorie_count: int
+
+class FoodTrack(BaseModel):
+    email: EmailStr
+    eaten_food: Dict[date, List[EachFood]] = {}
+
+
+# Track activites
+class Activities(str, Enum):
+    RUN = "run"
+    BIKE = "bike"
+    WALK = "walk"
+    BASKETBALL = "basketball"
+    SOCCER = "soccer"
+    WEIGHT_LIFT = "weight lift"
+    TENNIS = "tennis"
+    SWIM = "swim"
+    FOOTBALL = "football"
+    BASEBALL = "baseball"
+    HIKE = "hike"
+    VOLLEYBALL = "volleyball"
+    GOLF = "golf"
+    HIIT = "high intensity interval training"
+    SKATING = "skating"
+    DANCING = "dance"
+
+class EachActivity(BaseModel):
+    time: datetime
+    type: Activities
+    duration: float
+    distance: float
+
+class ActivityTrack(BaseModel):
+    email: EmailStr
+    activities_done: Dict[date, List[EachActivity]] = {}
---
+++ back_end/database/mongodb_connection.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+load_dotenv(find_dotenv())
+
+password = os.environ.get("MONGODB_PWD")
+uri = f"mongodb+srv://benboben03:{password}@dietpow.duheyjf.mongodb.net/?retryWrites=true&w=majority&appName=DietPow"
+# uri = "mongodb://localhost:27017/"
+client = AsyncIOMotorClient(uri)
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
