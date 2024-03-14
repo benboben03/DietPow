77f66845e0a915bb49e487dc853fe5863f7cac73
Reece Buyan <rbuyan@uci.edu>
2024-03-13 23:28:25 -0700

    Created new picker components for AdvancedInfoInput

---
+++ front_end/components/pickers/GoalPicker.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import {StyleSheet, Text, TouchableOpacity, View, ScrollView, Dimensions} from 'react-native';
+
+const OPTIONS = ['lose weight', 'maintain weight', 'gain weight']
+
+const WIDTH = Dimensions.get('window').width;
+const HEIGHT = Dimensions.get('window').height;
+const AgePicker = (props) => {
+    const onPressItem = (option) => {
+        props.changeModalVisibility(false);
+        props.setData(option);
+    }
+
+    const option = OPTIONS.map((item, index) => {
+        return (
+            <TouchableOpacity
+                style={styles.option}
+                key={index}
+                onPress={() => onPressItem(item)}
+            >
+                <Text style={styles.text}>
+                    {item}
+                </Text>
+            </TouchableOpacity>
+        )
+    })
+
+    return (
+        <TouchableOpacity
+            onPress={() => props.changeModalVisibility(false)}
+            style={styles.container}
+        >
+            <View style={[styles.modal, {width: WIDTH - 300, height: HEIGHT / 2}]}>
+                <ScrollView>
+                    {option}
+                </ScrollView>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modal: {
+        backgroundColor: '#f3f3f3',
+        borderRadius: 10
+    },
+    option: {
+        alignItems: 'flex-start',
+    },
+    text: {
+        margin: 20,
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+})
+
+export {AgePicker}
